df4351c34bbcc42b334ca088bfbd675929d56fb3
renatohsilva <re_hsilva@hotmail.com>
2021-02-08 01:25:36 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Microsoft.Azure.CognitiveServices.Samples.Face
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add your Azure Computer Vision subscription key and endpoint to your environment variables
+            string subscriptionKey = "3e045bf89e9144dbb2725746829dc135";
+            string endpoint = "https://biometriafacial.cognitiveservices.azure.com/";
+
+            // Detect sample.
+            Detection.Run(endpoint, subscriptionKey).Wait();
+
+            // FindSimilar samples.
+            FindSimilarInFaceIds.Run(endpoint, subscriptionKey).Wait();
+            FindSimilarInFaceList.Run(endpoint, subscriptionKey).Wait();
+            FindSimilarInLargeFaceList.Run(endpoint, subscriptionKey).Wait();
+
+            // Group sample.
+            Group.Run(endpoint, subscriptionKey).Wait();
+
+            // Identify sample.
+            IdentifyInPersonGroup.Run(endpoint, subscriptionKey).Wait();
+            IdentifyInLargePersonGroup.Run(endpoint, subscriptionKey).Wait();
+
+            // Verify samples.
+            VerifyFaceToFace.Run(endpoint, subscriptionKey).Wait();
+            VerifyInPersonGroup.Run(endpoint, subscriptionKey).Wait();
+            VerifyInLargePersonGroup.Run(endpoint, subscriptionKey).Wait();
+
+            Console.WriteLine("\nPress ENTER to exit.");
+            Console.ReadLine();
+        }
+    }
+}
